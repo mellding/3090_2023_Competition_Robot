7cbf2d54943997dddb542bc7f8e2b0369de37ba0
mellding <45488882+mellding@users.noreply.github.com>
2023-01-15 05:44:22 -0600

    updated drivestrain subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.PubSub;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -15,17 +16,36 @@ package frc.robot;
  */
 public final class Constants {
   public static class DriveTrainConstants {
+
+    public static final int PIGEAN_IMU_CAN_ID             = 9;
     public static final int LEFT_PRIMARY_CAN_ID           = 10;
     public static final int LEFT_SECONDARY_CAN_ID         = 11;
     public static final int RIGHT_PRIMARY_CAN_ID          = 12;
     public static final int RIGHT_SECONDARY_CAN_ID        = 13;
+    public static final int MOTOR_ENCODER_CPR             = 42;
+    public static final int MOTOR_CURRENT_LIMIT           = 40;
 
-    public static final int PIGEAN_IMU_CAN_ID             = 9;
+    public static final double OPEN_LOOP_RAMP_RATE        = 1.0;
+    public static final double SLOW_DRIVE_MAX             = 0.375;
+    public static final double WHEEL_DIAMETER_IN          = 6.0;
+    public static final double WHEEL_DIAMETER_FT          = WHEEL_DIAMETER_IN / 12.0;
+    public static final double WHEEL_CIRCUMFRENCE_IN      = Math.PI * WHEEL_DIAMETER_IN;
+    public static final double WHEEL_CIRCUMFRENCE_FT      = Math.PI * WHEEL_DIAMETER_FT;
+    public static final double DRIVE_GEAR_RATIO           = 8.46;
+    public static final double DRIVE_TRAIN_WIDTH          = 27;//????
+
+    public static final double LEFT_KP                    = 0.01;
+    public static final double LEFT_KI                    = 0.0;
+    public static final double LEFT_KD                    = 0.0;
+
+    public static final double RIGHT_KP                   = 0.01;
+    public static final double RIGHT_KI                   = 0.0;
+    public static final double RIGHT_KD                   = 0.0;
+
+
   }
 
   public static class OperatorConstants {
-
-
     public static final int kDriverControllerPort = 0;
   }
 
--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,15 +15,16 @@ import frc.robot.Constants;
 
 public class DriveTrainSubsystem extends SubsystemBase {
   /** Creates a new DriveTrainSubsystem. */
+  private static final Pigeon2  pigeonIMU = new Pigeon2(Constants.DriveTrainConstants.PIGEAN_IMU_CAN_ID);
   private static final CANSparkMax leftPrimary = new CANSparkMax(Constants.DriveTrainConstants.LEFT_PRIMARY_CAN_ID, MotorType.kBrushless);
   private static final CANSparkMax leftSecondary = new CANSparkMax(Constants.DriveTrainConstants.LEFT_SECONDARY_CAN_ID, MotorType.kBrushless);
-  private static final CANSparkMax rightPriary = new CANSparkMax(Constants.DriveTrainConstants.RIGHT_PRIMARY_CAN_ID, MotorType.kBrushless);
+  private static final CANSparkMax rightPrimary = new CANSparkMax(Constants.DriveTrainConstants.RIGHT_PRIMARY_CAN_ID, MotorType.kBrushless);
   private static final CANSparkMax rightSecondary = new CANSparkMax(Constants.DriveTrainConstants.RIGHT_SECONDARY_CAN_ID, MotorType.kBrushless);
-
-  private static final Pigeon2  pigeonIMU = new Pigeon2(Constants.DriveTrainConstants.PIGEAN_IMU_CAN_ID);
+  private static RelativeEncoder leftEncoder;
+  private static RelativeEncoder rightEncoder;
 
   public DriveTrainSubsystem() {
-    leftPrimary.restoreFactoryDefaults();       leftSecondary.restoreFactoryDefaults();
+    leftPrimary.restoreFactoryDefaults();      leftSecondary.restoreFactoryDefaults();
     rightPrimary.restoreFactoryDefaults();      rightSecondary.restoreFactoryDefaults();
 
     leftPrimary.setIdleMode(IdleMode.kCoast);   leftSecondary.setIdleMode(IdleMode.kCoast);
@@ -29,20 +32,44 @@ public class DriveTrainSubsystem extends SubsystemBase {
 
     leftSecondary.follow(leftPrimary); rightSecondary.follow(rightPrimary);
 
-    leftPrimary.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE); rightPrimary.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE);
+    leftPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
+    rightPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
 
-    leftPrimary.getEncoder(); rightPrimary.getEncoder();
+    leftPrimary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    leftSecondary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    rightPrimary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    rightSecondary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+
+    leftEncoder = leftPrimary.getEncoder(); 
+    rightEncoder = rightPrimary.getEncoder();
+
+    leftEncoder.setPosition(0.0);
+    rightEncoder.setPosition(0.0);
 
     leftPrimary.burnFlash(); leftSecondary.burnFlash();
     rightPrimary.burnFlash(); rightSecondary.burnFlash();
-
   }
 
   public static void GTA_Drive(double leftPower, double rightPower, double turn){
     leftPrimary.setIdleMode(IdleMode.kCoast);   leftSecondary.setIdleMode(IdleMode.kCoast);
     rightPrimary.setIdleMode(IdleMode.kCoast);  rightSecondary.setIdleMode(IdleMode.kCoast);
-    leftPrimary.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE); rightPrimary.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE);
+    leftPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
+    rightPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
     setMotors((rightPower - leftPower) - turn, (rightPower - leftPower) + turn);
+  }
+
+  public static void GTA_Drive_Slow(double leftPower, double rightPower, double turn){
+    leftPrimary.setIdleMode(IdleMode.kCoast);   leftSecondary.setIdleMode(IdleMode.kCoast);
+    rightPrimary.setIdleMode(IdleMode.kCoast);  rightSecondary.setIdleMode(IdleMode.kCoast);
+    leftPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
+    rightPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
+    double rightPowerSlow = mapDouble((rightPower - leftPower) + turn, -2, 2, 
+                                        -Constants.DriveTrainConstants.SLOW_DRIVE_MAX, 
+                                         Constants.DriveTrainConstants.SLOW_DRIVE_MAX);
+    double leftPowerSlow = mapDouble((rightPower - leftPower) - turn, -2, 2, 
+                                        -Constants.DriveTrainConstants.SLOW_DRIVE_MAX, 
+                                         Constants.DriveTrainConstants.SLOW_DRIVE_MAX);
+    setMotors(leftPowerSlow, rightPowerSlow);
   }
 
   public static void setMotors(double leftPower, double rightPower){
@@ -50,8 +77,29 @@ public class DriveTrainSubsystem extends SubsystemBase {
   }
 
   public static void setBrake(){
-    leftPrimary.setIdleMode(IdleMode.kBrake);   leftSecondary.setIdleMode(IdleMode.kCoast);
-    rightPrimary.setIdleMode(IdleMode.kCoast);  rightSecondary.setIdleMode(IdleMode.kCoast);
+    leftPrimary.setIdleMode(IdleMode.kBrake);   leftSecondary.setIdleMode(IdleMode.kBrake);
+    rightPrimary.setIdleMode(IdleMode.kBrake);  rightSecondary.setIdleMode(IdleMode.kBrake);
+    leftPrimary.setOpenLoopRampRate(0); 
+    rightPrimary.setOpenLoopRampRate(0);
+    setMotors(0, 0);
+  }
+
+  public static double getLeftPositionFt(){
+    return (leftEncoder.getPosition() / Constants.DriveTrainConstants.MOTOR_ENCODER_CPR) 
+                * Constants.DriveTrainConstants.WHEEL_CIRCUMFRENCE_FT;
+  }
+
+  public static double getRightPositionFt(){
+    return (rightEncoder.getPosition() / Constants.DriveTrainConstants.MOTOR_ENCODER_CPR) 
+                * Constants.DriveTrainConstants.WHEEL_CIRCUMFRENCE_FT;
+  }
+
+  public static double getYaw(){return pigeonIMU.getYaw();}
+  public static double getPitch(){return pigeonIMU.getPitch();}
+  public static double getRoll(){return pigeonIMU.getRoll();}
+
+  private static double mapDouble(double valueIn, double baseMin, double baseMax, double limitMin, double limitMax) {
+    return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin;
   }
 
   @Override
