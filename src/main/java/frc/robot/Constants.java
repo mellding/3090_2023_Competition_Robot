b9e0f46c7bd51bfab020a151d5080d656240a0f4
mellding <45488882+mellding@users.noreply.github.com>
2023-01-17 03:15:51 -0600

    odometry

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,17 +33,20 @@ public final class Constants {
     public static final double WHEEL_CIRCUMFRENCE_FT      = Math.PI * WHEEL_DIAMETER_FT;
     public static final double DRIVE_GEAR_RATIO           = 8.46;
     public static final double DRIVE_TRAIN_WIDTH          = 27;//????
+    public static final double ENCODER_CONVERSION_FACTOR  = 1/DRIVE_GEAR_RATIO * WHEEL_CIRCUMFRENCE_FT;
 
     public static final double LEFT_KP                    = 0.01;
     public static final double LEFT_KI                    = 0.0;
     public static final double LEFT_KD                    = 0.0;
     public static final double LEFT_KFF                   = 0.0;
-
+    
     public static final double RIGHT_KP                   = 0.01;
     public static final double RIGHT_KI                   = 0.0;
     public static final double RIGHT_KD                   = 0.0;
-    public static final double RIGHT_KFF                    = 0.0;
-
+    public static final double RIGHT_KFF                  = 0.0;
+    
+    public static final double PID_MAX                    = 1.0;
+    public static final double CLOSED_LOOP_RAMP           = 2.0;
 
   }
 
--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -11,19 +11,22 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.DriveTrainConstants;
 
 public class DriveTrainSubsystem extends SubsystemBase {
   /** Creates a new DriveTrainSubsystem. */
-  private static final Pigeon2  pigeonIMU = new Pigeon2(Constants.DriveTrainConstants.PIGEAN_IMU_CAN_ID);
-  private static final CANSparkMax leftPrimary = new CANSparkMax(Constants.DriveTrainConstants.LEFT_PRIMARY_CAN_ID, MotorType.kBrushless);
-  private static final CANSparkMax leftSecondary = new CANSparkMax(Constants.DriveTrainConstants.LEFT_SECONDARY_CAN_ID, MotorType.kBrushless);
-  private static final CANSparkMax rightPrimary = new CANSparkMax(Constants.DriveTrainConstants.RIGHT_PRIMARY_CAN_ID, MotorType.kBrushless);
-  private static final CANSparkMax rightSecondary = new CANSparkMax(Constants.DriveTrainConstants.RIGHT_SECONDARY_CAN_ID, MotorType.kBrushless);
+  private static final Pigeon2  pigeonIMU = new Pigeon2(DriveTrainConstants.PIGEAN_IMU_CAN_ID);
+  private static final CANSparkMax leftPrimary = new CANSparkMax(DriveTrainConstants.LEFT_PRIMARY_CAN_ID, MotorType.kBrushless);
+  private static final CANSparkMax leftSecondary = new CANSparkMax(DriveTrainConstants.LEFT_SECONDARY_CAN_ID, MotorType.kBrushless);
+  private static final CANSparkMax rightPrimary = new CANSparkMax(DriveTrainConstants.RIGHT_PRIMARY_CAN_ID, MotorType.kBrushless);
+  private static final CANSparkMax rightSecondary = new CANSparkMax(DriveTrainConstants.RIGHT_SECONDARY_CAN_ID, MotorType.kBrushless);
   private final  DifferentialDriveOdometry driveOdometry;
   private static DifferentialDrive diffDrive;
   private static RelativeEncoder leftEncoder;
@@ -31,8 +34,10 @@ public class DriveTrainSubsystem extends SubsystemBase {
   private static SparkMaxPIDController leftPID;
   private static SparkMaxPIDController rightPID;
 
+ 
   private static double leftMaxCurrent = 0.0;
   private static double rightMaxCurrent = 0.0;
+
 
   public DriveTrainSubsystem() {
     leftPrimary.restoreFactoryDefaults();      leftSecondary.restoreFactoryDefaults();
@@ -46,40 +51,55 @@ public class DriveTrainSubsystem extends SubsystemBase {
 
     leftSecondary.follow(leftPrimary); rightSecondary.follow(rightPrimary);
 
-    leftPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
-    rightPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
+    leftPrimary.setOpenLoopRampRate(DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
+    rightPrimary.setOpenLoopRampRate(DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
+    leftPrimary.setClosedLoopRampRate(DriveTrainConstants.CLOSED_LOOP_RAMP);
+    rightPrimary.setClosedLoopRampRate(DriveTrainConstants.CLOSED_LOOP_RAMP);
 
-    leftPrimary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
-    leftSecondary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
-    rightPrimary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
-    rightSecondary.setSmartCurrentLimit(Constants.DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    leftPrimary.setSmartCurrentLimit(DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    leftSecondary.setSmartCurrentLimit(DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    rightPrimary.setSmartCurrentLimit(DriveTrainConstants.MOTOR_CURRENT_LIMIT);
+    rightSecondary.setSmartCurrentLimit(DriveTrainConstants.MOTOR_CURRENT_LIMIT);
 
-    leftEncoder.setPosition(0.0);
-    rightEncoder.setPosition(0.0);
-
-    leftEncoder = leftPrimary.getEncoder(); 
-    rightEncoder = rightPrimary.getEncoder();
-
+    
+    leftEncoder = leftPrimary.getEncoder(); rightEncoder = rightPrimary.getEncoder();
+    
     leftEncoder.setInverted(false); rightEncoder.setInverted(true);
+    
+    leftEncoder.setPosition(0.0); rightEncoder.setPosition(0.0);
+    
+    leftEncoder.setPositionConversionFactor(DriveTrainConstants.ENCODER_CONVERSION_FACTOR);
+    rightEncoder.setPositionConversionFactor(DriveTrainConstants.ENCODER_CONVERSION_FACTOR);
+    leftEncoder.setVelocityConversionFactor(DriveTrainConstants.ENCODER_CONVERSION_FACTOR/60);
+    rightEncoder.setVelocityConversionFactor(DriveTrainConstants.ENCODER_CONVERSION_FACTOR/60);
 
     leftPID = leftPrimary.getPIDController();
     rightPID = rightPrimary.getPIDController();
 
-    leftPID.setP(Constants.DriveTrainConstants.LEFT_KP);
-    leftPID.setI(Constants.DriveTrainConstants.LEFT_KI);
-    leftPID.setD(Constants.DriveTrainConstants.LEFT_KD);
-    leftPID.setFF(Constants.DriveTrainConstants.LEFT_KFF);
+    leftPID.setP(DriveTrainConstants.LEFT_KP);
+    leftPID.setI(DriveTrainConstants.LEFT_KI);
+    leftPID.setD(DriveTrainConstants.LEFT_KD);
+    leftPID.setFF(DriveTrainConstants.LEFT_KFF);
+    leftPID.setOutputRange(-DriveTrainConstants.PID_MAX, DriveTrainConstants.PID_MAX);
 
-    rightPID.setP(Constants.DriveTrainConstants.RIGHT_KP);
-    rightPID.setI(Constants.DriveTrainConstants.RIGHT_KI);
-    rightPID.setD(Constants.DriveTrainConstants.RIGHT_KD);
-    rightPID.setFF(Constants.DriveTrainConstants.RIGHT_KFF);
+    rightPID.setP(DriveTrainConstants.RIGHT_KP);
+    rightPID.setI(DriveTrainConstants.RIGHT_KI);
+    rightPID.setD(DriveTrainConstants.RIGHT_KD);
+    rightPID.setFF(DriveTrainConstants.RIGHT_KFF);
+    rightPID.setOutputRange(-DriveTrainConstants.PID_MAX, DriveTrainConstants.PID_MAX);
 
     leftPrimary.burnFlash(); leftSecondary.burnFlash();
     rightPrimary.burnFlash(); rightSecondary.burnFlash();
 
     diffDrive = new DifferentialDrive(leftPrimary, rightPrimary);
-    driveOdometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getYaw()), getLeftPositionFt(), getRightPositionFt());
+    driveOdometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getYaw()), 
+                                                    leftEncoder.getPosition(), 
+                                                    rightEncoder.getPosition());
+
+    driveOdometry.resetPosition(Rotation2d.fromDegrees(getYaw()), 
+                                  leftEncoder.getPosition(), 
+                                  rightEncoder.getPosition(), 
+                                  new Pose2d());
   }
 
   public static void GTA_Drive(double leftPower, double rightPower, double turn){
@@ -90,11 +110,11 @@ public class DriveTrainSubsystem extends SubsystemBase {
   public static void GTA_Drive_Slow(double leftPower, double rightPower, double turn){
     setCoast();
     double rightPowerSlow = mapDouble((rightPower - leftPower) + turn, -2, 2, 
-                                        -Constants.DriveTrainConstants.SLOW_DRIVE_MAX, 
-                                         Constants.DriveTrainConstants.SLOW_DRIVE_MAX);
+                                        -DriveTrainConstants.SLOW_DRIVE_MAX, 
+                                         DriveTrainConstants.SLOW_DRIVE_MAX);
     double leftPowerSlow = mapDouble((rightPower - leftPower) - turn, -2, 2, 
-                                        -Constants.DriveTrainConstants.SLOW_DRIVE_MAX, 
-                                         Constants.DriveTrainConstants.SLOW_DRIVE_MAX);
+                                        -DriveTrainConstants.SLOW_DRIVE_MAX, 
+                                         DriveTrainConstants.SLOW_DRIVE_MAX);
     setMotors(leftPowerSlow, rightPowerSlow);
   }
 
@@ -105,8 +125,8 @@ public class DriveTrainSubsystem extends SubsystemBase {
   public static void setCoast(){
     leftPrimary.setIdleMode(IdleMode.kCoast);   leftSecondary.setIdleMode(IdleMode.kCoast);
     rightPrimary.setIdleMode(IdleMode.kCoast);  rightSecondary.setIdleMode(IdleMode.kCoast);
-    leftPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
-    rightPrimary.setOpenLoopRampRate(Constants.DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
+    leftPrimary.setOpenLoopRampRate(DriveTrainConstants.OPEN_LOOP_RAMP_RATE); 
+    rightPrimary.setOpenLoopRampRate(DriveTrainConstants.OPEN_LOOP_RAMP_RATE);
   }
 
   public static void setBrake(){
@@ -116,19 +136,11 @@ public class DriveTrainSubsystem extends SubsystemBase {
     rightPrimary.setOpenLoopRampRate(0);
     setMotors(0, 0);
   }
-
-  public static double getLeftPositionFt(){
-    return ((leftEncoder.getPosition() / Constants.DriveTrainConstants.MOTOR_ENCODER_CPR) 
-                / Constants.DriveTrainConstants.DRIVE_GEAR_RATIO)
-                * Constants.DriveTrainConstants.WHEEL_CIRCUMFRENCE_FT;
-  }
-
-  public static double getRightPositionFt(){
-    return ((rightEncoder.getPosition() / Constants.DriveTrainConstants.MOTOR_ENCODER_CPR) 
-                / Constants.DriveTrainConstants.DRIVE_GEAR_RATIO) 
-                * Constants.DriveTrainConstants.WHEEL_CIRCUMFRENCE_FT;
-  }
   
+  public static double getLeftPosition(){return leftEncoder.getPosition();}
+  public static double getRightPosition(){return rightEncoder.getPosition();}
+  public static double getLeftVelocity(){return leftEncoder.getVelocity();}
+  public static double getRightVelovity(){return rightEncoder.getVelocity();}
   public static double getLeftCurrent(){return (leftPrimary.getOutputCurrent() + leftSecondary.getOutputCurrent()) / 2;}
   public static double getRightCurrent(){return (rightPrimary.getOutputCurrent() + rightSecondary.getOutputCurrent()) / 2;}
   public static double getLeftMaxCurrent(){return leftMaxCurrent;}
@@ -146,6 +158,12 @@ public class DriveTrainSubsystem extends SubsystemBase {
     if(getLeftCurrent() > leftMaxCurrent) leftMaxCurrent = getLeftCurrent();
     if(getRightCurrent() > rightMaxCurrent) rightMaxCurrent = getRightCurrent();
 
-    driveOdometry.update(Rotation2d.fromDegrees(getYaw()), getLeftPositionFt(), getRightPositionFt());
+    driveOdometry.update(Rotation2d.fromDegrees(getYaw()), leftEncoder.getPosition(), rightEncoder.getPosition());
+
+    SmartDashboard.putNumber("Left Position", leftEncoder.getPosition());
+    SmartDashboard.putNumber("Right Position", rightEncoder.getPosition());
+    SmartDashboard.putNumber("Yaw", pigeonIMU.getYaw());
+
+    
   }
 }
